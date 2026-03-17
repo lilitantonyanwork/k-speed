61d632ddb9437c5da39db5f9a9f3643720a6e5b4
lilitantonyan <lilit@helix.am>
2026-03-17 14:20:57 +0400

    add modal

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     menu_btn.addEventListener('click', function (){
         menu.classList.toggle('active');
     })
+    const modal__btn = document.querySelector('.banner .btn__red');
+    const modal = document.querySelector('.modal');
+    const modal__close = document.querySelector('.modal__close');
+    const body = document.querySelector('body');
+    modal__btn.addEventListener('click', function (e){
+        e.preventDefault();
+        modal.classList.toggle('open');
+        body.classList.toggle('no-scroll');
+    })
+    modal__close.addEventListener('click', function (){
+        modal.classList.toggle('open');
+        body.classList.toggle('no-scroll');
 
-
+    })
 });
